d561fecaa2b6d4f07cee286961b2bce033eeeb08
hiimchinh <chinhhm@proton.me>
2025-01-28 23:32:18 +0700

    add function convert text node to html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,22 @@
+from leafnode import LeafNode
+from textnode import TextType
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode('code', text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode('a', text_node.text, {'href': text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode('img', None, {'src': text_node.url, 'alt': text_node.text})
+    raise TypeError('text type is not supported')
+
+
 class HTMLNode:
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
